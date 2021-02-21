858cf57b7a6dfeacc0d0aa50755b1dd46204dc7c
Messmer-Landon <messmer_landon@bah.com>
2021-02-20 20:42:56 -0500

    yes

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ThemeDetection } from '@ionic-native/theme-detection';
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
