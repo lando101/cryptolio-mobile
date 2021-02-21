503e53a85efe27f7d35b3b50095861c002f7ed56
Messmer-Landon <messmer_landon@bah.com>
2021-02-20 23:52:10 -0500

    stuff

--- src/app/crypto-assets-tab/crypto-assets-tab.module.ts
+++ src/app/crypto-assets-tab/crypto-assets-tab.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CryptoAssetsTabRoutingModule } from './crypto-assets-tab-routing.module';
 import { CryptoAssetsTabComponent } from './crypto-assets-tab.component';
 import { IonicModule } from '@ionic/angular';
+import { CryptoInfoModalComponent } from './crypto-info-modal/crypto-info-modal.component';
 
 @NgModule({
-  declarations: [CryptoAssetsTabComponent],
+  declarations: [CryptoAssetsTabComponent, CryptoInfoModalComponent],
   imports: [CommonModule, CryptoAssetsTabRoutingModule, IonicModule],
 })
 export class CryptoAssetsTabModule {}
